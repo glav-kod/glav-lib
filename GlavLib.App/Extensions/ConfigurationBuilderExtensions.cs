fa1df94211d829763bed461f248361abbb0b653a
Oleg Meshechkov <omeshechkov@gmail.com>
2023-12-29 19:18:38 +0600

    ConfigurationBuilderExtensions

---
+++ GlavLib.App/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GlavLib.App.Extensions;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static TConfigurationBuilder UseCustomAppSettings<TConfigurationBuilder>(this TConfigurationBuilder configurationBuilder)
+        where TConfigurationBuilder : IConfigurationBuilder
+    {
+        configurationBuilder.Sources.Clear();
+        configurationBuilder.AddJsonFile("appsettings.base.json", optional: false);
+        configurationBuilder.AddJsonFile("appsettings.json", optional: true);
+        configurationBuilder.AddJsonFile("appsettings.local.json", optional: true);
+        configurationBuilder.AddJsonFile("appsettings.production.json", optional: true);
+        configurationBuilder.AddEnvironmentVariables();
+
+        return configurationBuilder;
+    }
+}

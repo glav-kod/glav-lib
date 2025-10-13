cba07fdb92245e3b2149c61e4843012a42a80723
Oleg Meshechkov <omeshechkov@gmail.com>
2025-10-13 15:01:32 +0600

    Добавил расширение Dapper для загрузки EnumObject-мапперов

---
+++ GlavLib.Db/Dapper/DapperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Dapper;
+using GlavLib.Abstractions.DataTypes;
+
+namespace GlavLib.Db.Dapper;
+
+public static class DapperExtensions
+{
+    public static void AddEnumObjectTypeHandlers(Assembly assembly)
+    {
+        var types           = assembly.GetTypes();
+        var enumObjectTypes = types.Where(t => t.IsSubclassOf(typeof(EnumObject)));
+
+        foreach (var enumObjectType in enumObjectTypes)
+        {
+            var handlerType = typeof(EnumObjectTypeHandler<>).MakeGenericType(enumObjectType);
+            var handler     = (SqlMapper.ITypeHandler)Activator.CreateInstance(handlerType)!;
+
+            SqlMapper.AddTypeHandler(enumObjectType, handler);
+        }
+    }
+}
---
+++ GlavLib.Db/Dapper/EnumObjectTypeHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using Dapper;
+using GlavLib.Abstractions.DataTypes;
+
+namespace GlavLib.Db.Dapper;
+
+public sealed class EnumObjectTypeHandler<TEnumObject> : SqlMapper.ITypeHandler
+    where TEnumObject : IEnumObject<TEnumObject>
+
+{
+    public void SetValue(IDbDataParameter parameter, object value)
+    {
+        var enumObject = (TEnumObject)value;
+
+        parameter.Value = enumObject.Key;
+    }
+
+    public object Parse(Type destinationType, object value)
+    {
+        var key = value.ToString()!;
+
+        return TEnumObject.Create(key);
+    }
+}
